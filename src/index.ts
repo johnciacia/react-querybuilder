aeec149eb3b417515d97d7c57bdff83f54d8caf1
John Ciacia <johnciacia@users.noreply.github.com>
2020-09-16 00:42:42 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { QueryBuilder } from './QueryBuilder';
 export default QueryBuilder;
 export { formatQuery } from './utils';
 export { Rule } from './Rule';
+export { RuleGroup } from './RuleGroup';
 export * from './types';
